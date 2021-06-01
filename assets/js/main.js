359d9139b13b7cfc47539ce610d3be2e876fc42b
Romain Vinai <romain.vinai13009@gmail.com>
2021-06-01 11:48:00 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,43 @@
+// MENU
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId)
+    const nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show-menu')
+        })
+    }
+}
+
+showMenu('nav-toggle', 'nav-menu')
+
+// REMOVE MENU
+const navlink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+
+navlink.forEach(link => link.addEventListener('click', linkAction))
+
+// SCROLL SECTION
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
